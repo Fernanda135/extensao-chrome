5c2407393f040210aaef5816171d459feb153a4a
Fernanda Araújo <fernandacomun@gmail.com>
2025-04-21 10:14:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-let myLeads = []
+//variaveis
+let meusLinks = [] 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const linksLocalStorage = JSON.parse( localStorage.getItem("meusLinks") )
 const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+//carregar links salvos
+if (linksLocalStorage) {
+    meusLinks = linksLocalStorage
+    render(meusLinks)
 }
 
+//pegar link da janela atual
 tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active:  true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+        meusLinks.push(tabs[0].url)
+        localStorage.setItem("meusLinks", JSON.stringify(meusLinks) )
+        render(meusLinks)
     })
 })
 
-function render(leads) {
+//renderizar links
+function render(links) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${links[i]}'>
+                    ${links[i]}
                 </a>
             </li>
         `
@@ -33,15 +37,17 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+//deletar todos os links
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    meusLinks = []
+    render(meusLinks)
 })
 
+//adicionar link
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    meusLinks.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+    localStorage.setItem("meusLinks", JSON.stringify(meusLinks) )
+    render(meusLinks)
 })
